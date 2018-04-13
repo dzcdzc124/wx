9e56399c738cb6f42f070e9f6c851229067a62ce
dzcdzc124 <dengzhengcai@vivo.com.cn>
2018-04-13 17:28:36 +0800

    no message

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!doctype>
 <html>
     <head>
+        <meta http-equiv="Cache-Control" content="no-cache">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>秀个恩爱</title>
     </head>
     <body>
