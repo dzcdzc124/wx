631aab1a2b950714c060aa8e0d6d12e29bfa6304
dzcdzc124 <dengzhengcai@vivo.com.cn>
2018-04-14 11:04:15 +0800

    no message

--- index.php
+++ index.php
@@ -10,10 +10,25 @@
         <script>
             var hash = window.location.hash;
             if(hash == ""){
-                window.location.href = "https://www.baidu.com";
+                var link = getQueryString("link");
+                window.location.href = unescape(link);
             }else{
                 window.location.hash = "";
+                var title = getQueryString("title");
+                document.title = title;
             }
+
+            //获取url参数
+            function getQueryString(name){
+                var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+                //解析中文
+                var link = decodeURI(window.location.search);
+                var r = link.substr(1).match(reg);
+                if( r!=null )
+                    return  unescape(r[2]);
+                return null;
+            }
+
         </script>
     </body>
 </html>
